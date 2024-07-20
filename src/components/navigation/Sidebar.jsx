1287b2751cecc0c6e104cd9f62d3bcfaa4a72fc2
i3han <ishanavishka14@gmail.com>
2024-07-20 18:51:50 +0530

    update styles

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -16,8 +16,8 @@ function Sidebar() {
   );
 
   return (
-    <div  className="flex flex-col">
-      <div className="w-20 h-20 items-center justify-center text-center border-r border-r-gray-200 px-2 py-4">
+    <div  className="flex flex-col h-screen border-r border-r-gray-200">
+      <div className="w-20 h-20 items-center justify-center text-center px-2 py-4">
         <img
           src={AffoohLogo}
           alt="Affooh Logo"
@@ -25,7 +25,7 @@ function Sidebar() {
         />
       </div>
 
-      <div className="w-20 text-white flex flex-col items-center py-5 space-y-6 border-r border-r-gray-200">
+      <div className="w-20 text-white flex flex-col items-center py-5 space-y-6">
         <MenuItem link="/dashboard" Icon={HomeIcon}/>
         <MenuItem link="/projects" Icon={ClipboardIcon}/>
         <MenuItem link="/profile" Icon={UserIcon}/>
