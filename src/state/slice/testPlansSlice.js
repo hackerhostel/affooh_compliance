6f03252c5880392c042b4b125ff83ccaa94b22c3
Malindu <malindu@missamara.com.au>
2024-08-22 18:19:16 +0530

    Test Plans #1

---
+++ src/state/slice/testPlansSlice.js
@@ -0,0 +1,59 @@
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import axios from "axios";
+
+const initialState = {
+    isTestPlanListForProjectError: false,
+    isTestPlanListForProjectLoading: false,
+    testPlanListForProject: [],
+};
+
+export const doGetTestPlans = createAsyncThunk(
+    'testPlans/getTestPlans',
+    async (projectId, thunkApi) => {
+        try {
+            const response = await axios.get(`/projects/${projectId}/test-plans`)
+            const responseData = response.data.body;
+
+            if (responseData) {
+                return responseData
+            } else {
+                return thunkApi.rejectWithValue('test plans not found');
+            }
+        } catch (error) {
+            console.log(error)
+            return thunkApi.rejectWithValue(error.message);
+        }
+    }
+);
+
+export const testPlansSlice = createSlice({
+    name: 'testPlans',
+    initialState,
+    reducers: {
+        clearTestPlanState: () => initialState,
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(doGetTestPlans.pending, (state) => {
+                state.isTestPlanListForProjectLoading = true;
+            })
+            .addCase(doGetTestPlans.fulfilled, (state, action) => {
+                console.log(action.payload)
+                state.testPlanListForProject = action.payload;
+                state.isTestPlanListForProjectLoading = false;
+                state.isTestPlanListForProjectError = false;
+            })
+            .addCase(doGetTestPlans.rejected, (state, action) => {
+                state.isTestPlanListForProjectLoading = false;
+                state.isTestPlanListForProjectError = true;
+            });
+    },
+});
+
+export const {clearTestPlanState} = testPlansSlice.actions;
+
+export const selectIsTestPlanListForProjectError = (state) => state?.testPlans?.isTestPlanListForProjectError;
+export const selectIsTestPlanListForProjectLoading = (state) => state?.testPlans?.isTestPlanListForProjectLoading;
+export const selectTestPlanListForProject = (state) => state?.testPlans?.testPlanListForProject;
+
+export default testPlansSlice.reducer;
