3a5b580701f3455191434690dd7596b949cfd93f
i3han <ishanavishka14@gmail.com>
2024-09-01 23:18:44 +0530

    task list changes

--- src/components/sprint-table/index.jsx
+++ src/components/sprint-table/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import DataGrid, {
   Column,
   SearchPanel,
@@ -14,7 +14,7 @@ const SprintTable = ({taskList}) => {
 
   const taskTitleComponent = (data) => {
     return <button
-      className="px-2 py-1 text-sm"
+      className="px-2 py-1 text-sm hover:bg-gray-50 rounded-lg"
       onClick={() => {
         history.push(`/task/${data?.key?.taskCode}`);
       }}
@@ -34,6 +34,14 @@ const SprintTable = ({taskList}) => {
     return <div className="font-bold text-gray-600">{data.column.caption}</div>;
   };
 
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight  - 200);
+
+  useEffect(() => {
+    const handleResize = () => setWindowHeight(window.innerHeight  - 200);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className="p-4">
       <DataGrid
@@ -43,13 +51,14 @@ const SprintTable = ({taskList}) => {
         showBorders={true}
         width="100%"
         className="shadow-lg rounded-lg overflow-hidden sprint-grid-table"
+        height={windowHeight}
       >
         <GroupPanel visible/>
         <SearchPanel visible/>
         <Grouping autoExpandAll/>
         <Paging enabled={false}/>
         <ColumnChooser enabled={true} mode="select"/>
-        <Scrolling mode="standard" useNative={true}/>
+        <Scrolling columnRenderingMode="virtual" />
         <Sorting mode="multiple"/>
 
         <Column
@@ -61,6 +70,7 @@ const SprintTable = ({taskList}) => {
         <Column
           dataField="title"
           caption="Title"
+          width={500}
           headerCellRender={customHeaderRender}
           cellRender={taskTitleComponent}
         />
