4fd1201786285cf3324d3da14b3eb1d82819919a
yasasn <yasastharindu15@gmail.com>
2025-01-08 16:06:34 +0530

    refactored to fix es-lint errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,63 @@
-import {useState} from 'react'
-import './App.css'
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import ProjectListPage from "./pages/project-page/ProjectListPage.jsx";
-import Login from "./auth/Login.jsx";
-import Register from './auth/Register.jsx'
-import ForgotPassword from './auth/ForgotPassword.jsx'
-import PublicGuard from "./auth/PublicGuard.jsx";
-import AuthGuard from "./auth/AuthGuard.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
-import OTPVerification from "./auth/OTPVerification.jsx";
-import UserInviteRegister from "./auth/UserInviteRegister.jsx";
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import AuthGuard from './auth/AuthGuard';
+import ForgotPassword from './auth/ForgotPassword';
+import Login from './auth/Login';
+import OTPVerification from './auth/OTPVerification';
+import PublicGuard from './auth/PublicGuard';
+import Register from './auth/Register';
+import UserInviteRegister from './auth/UserInviteRegister';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login">
-            <PublicGuard>
-              <Login />
-            </PublicGuard>
-          </Route>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login">
+          <PublicGuard>
+            <Login />
+          </PublicGuard>
+        </Route>
 
-          <Route path="/register">
-            <PublicGuard>
-              <Register />
-            </PublicGuard>
-          </Route>
+        <Route path="/register">
+          <PublicGuard>
+            <Register />
+          </PublicGuard>
+        </Route>
 
-          <Route path="/inviteUserRegister">
-            <PublicGuard>
-              <UserInviteRegister />
-            </PublicGuard>
-          </Route>
+        <Route path="/inviteUserRegister">
+          <PublicGuard>
+            <UserInviteRegister />
+          </PublicGuard>
+        </Route>
 
-          <Route path="/otpVerification">
-            <PublicGuard>
-              <OTPVerification />
-            </PublicGuard>
-          </Route>
+        <Route path="/otpVerification">
+          <PublicGuard>
+            <OTPVerification />
+          </PublicGuard>
+        </Route>
 
-          <Route path="/forgot-password">
-            <PublicGuard>
-              {/*TODO: need to implement*/}
-              <ForgotPassword />
-            </PublicGuard>
-          </Route>
+        <Route path="/forgot-password">
+          <PublicGuard>
+            {/* TODO: need to implement */}
+            <ForgotPassword />
+          </PublicGuard>
+        </Route>
 
-          <Route path="/forgot-password-reset">
-            <PublicGuard>
-              {/*TODO: need to implement*/}
-              {/*<ForgotPasswordReset />*/}
-            </PublicGuard>
-          </Route>
+        <Route path="/forgot-password-reset">
+          <PublicGuard>
+            {/* TODO: need to implement */}
+            {/* <ForgotPasswordReset /> */}
+          </PublicGuard>
+        </Route>
 
-          <AuthGuard>
-            <Dashboard />
-          </AuthGuard>
-        </Switch>
-      </BrowserRouter>
-  )
+        <AuthGuard>
+          <Dashboard />
+        </AuthGuard>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
