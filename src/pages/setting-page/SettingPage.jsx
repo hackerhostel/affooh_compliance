511be3970f08bec76c18d9485af443ea59e8b369
NipunS12 <114478939+NipunS12@users.noreply.github.com>
2025-03-07 13:25:11 +0530

    update
    update

--- src/pages/setting-page/SettingPage.jsx
+++ src/pages/setting-page/SettingPage.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import {useSelector} from "react-redux";
-import {selectSelectedProject} from "../../state/slice/projectSlice.js";
-import CustomFieldsListPage from "./CustomFieldsListPage.jsx";
-import ScreenListPage from "./ScreenListPage.jsx";
-import TaskTypeListPage from "./TaskTypeListPage.jsx";
+import { useSelector } from "react-redux";
+import { selectSelectedProject } from "../../state/slice/projectSlice.js";
 
 const SettingPage = () => {
     const selectedProject = useSelector(selectSelectedProject);
 
     return (
-        <div className="h-list-screen overflow-y-auto w-full">
-            <CustomFieldsListPage/>
-            <ScreenListPage selectedProject={selectedProject}/>
-            <TaskTypeListPage selectedProject={selectedProject}/>
+        <div className="h-list-screen overflow-y-auto w-full flex flex-col space-y-2">
+            <button
+                style={{ width: "266px" }}
+                className={`flex justify-between items-center p-3 border rounded-md w-full gap-2 hover:bg-gray-100 cursor-pointer "border-primary-pink" : "border-gray-200"`}
+            >Custom fields</button>
+
+            <button
+                style={{ width: "266px" }}
+                className={`flex justify-between items-center p-3 border rounded-md w-full gap-2 hover:bg-gray-100 cursor-pointer "border-primary-pink" : "border-gray-200"`}
+            >Task Type</button>
+
+            <button
+                style={{ width: "266px" }}
+                className={`flex justify-between items-center p-3 border rounded-md w-full gap-2 hover:bg-gray-100 cursor-pointer "border-primary-pink" : "border-gray-200"`}
+            >Screens</button>
         </div>
     );
 };
