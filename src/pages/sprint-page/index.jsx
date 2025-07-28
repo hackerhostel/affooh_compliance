6c8132b5b1a5ef1717aae85034b887e3c52bfe8c
NipunS12 <114478939+NipunS12@users.noreply.github.com>
2025-07-28 13:05:56 +0530

    update
    update

--- src/pages/sprint-page/index.jsx
+++ src/pages/sprint-page/index.jsx
@@ -21,7 +21,7 @@ const SprintLayout = () => {
   return (
       <>
         <MainPageLayout
-            title={selectedProject?.projectType === 1 ? 'Gap Analysis ' : 'Kanban'}
+            title={selectedProject?.projectType === 1 ? 'Gap Analyse ' : 'Kanban'}
             onAction={onAddNew}
             subText={selectedProject?.projectType === 1 ? 'Add New' : ''}
             leftColumn={<GapAnalysisListPage/>}
