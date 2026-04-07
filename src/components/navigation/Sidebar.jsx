d548a3879ba04e701a2b7194b95054fe0346fe64
lakshan weerasingha <lakshanravindu9999@gmail.com>
2026-04-07 10:30:13 +0530

    logout-bug-fix

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -19,14 +19,15 @@ import {
 import { signOut } from 'aws-amplify/auth';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import AffoohLogo from '../../assets/affooh_logo.png';
-import { useSelector } from 'react-redux';
-import { selectUser } from '../../state/slice/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearAuthState, selectUser } from '../../state/slice/authSlice';
 import React, { Fragment, useState } from 'react';
 import Notification from "./NotificationPopup.jsx";
 
 function Sidebar() {
   const location = useLocation();
   const history = useHistory();
+  const dispatch = useDispatch();
   const userDetails = useSelector(selectUser);
   const [loading, setLoading] = useState(false);
   const [isOpenPopUp, setIsOpenPopUp] = useState(false);
@@ -45,12 +46,15 @@ function Sidebar() {
       // Set same-origin event
       localStorage.setItem('logout-event', Date.now().toString());
       
+      // Clear redux state
+      dispatch(clearAuthState());
+
       // Finally clear local logs
       localStorage.clear();
       sessionStorage.clear();
-      
-      // Removed window.location.href = "/auth" here because it overrides 
-      // the AWS Amplify Hosted UI automatic redirect to the Cognito logout URL.
+
+      // Redirect to login
+      window.location.href = "/login";
     } catch (err) {
       console.error("Logout failed", err);
     } finally {
