ae5c5540fbd539470a6bfa2a4266e852396fa6cf
lakshan weerasingha <lakshanravindu9999@gmail.com>
2026-04-22 14:23:27 +0530

    hide menu and come back highlight is disappear

--- src/layouts/MainPageLayout.jsx
+++ src/layouts/MainPageLayout.jsx
@@ -3,7 +3,7 @@ import {
   ChevronDoubleRightIcon,
   PlusCircleIcon,
 } from "@heroicons/react/24/outline/index.js";
-import React, { useState } from "react";
+import { useState } from "react";
 
 const MainPageLayout = ({
   title,
@@ -19,29 +19,27 @@ const MainPageLayout = ({
 
   return (
     <div className="grid grid-cols-5 h-full relative">
-      {isLeftColumnVisible && (
-        <div className="col-span-1 relative">
-          <div className="pl-4 mt-5 mb-10 flex items-center gap-6">
-            <span className="text-2xl font-medium">{title}</span>
-            {subText && (
-              <div
-                className="flex gap-1 items-center cursor-pointer"
-                onClick={onAction}
-              >
-                <PlusCircleIcon className="w-6 h-6 text-pink-500" />
-                <span className="font-thin text-xs text-gray-600">{subText}</span>
-              </div>
-            )}
-          </div>
-          <div className={"flex"}>{leftColumn}</div>
-          <div
-            className="absolute top-1/2 right-0 transform -translate-y-1/2 flex items-center justify-center mt-12 h-10 bg-white rounded-full shadow cursor-pointer"
-            onClick={hideLeftColumn}
-          >
-            <ChevronLeftIcon className={"w-4 h-4 text-pink-500"} />
-          </div>
+      <div className={`col-span-1 relative ${isLeftColumnVisible ? "" : "hidden"}`}>
+        <div className="pl-4 mt-5 mb-10 flex items-center gap-6">
+          <span className="text-2xl font-medium">{title}</span>
+          {subText && (
+            <div
+              className="flex gap-1 items-center cursor-pointer"
+              onClick={onAction}
+            >
+              <PlusCircleIcon className="w-6 h-6 text-pink-500" />
+              <span className="font-thin text-xs text-gray-600">{subText}</span>
+            </div>
+          )}
         </div>
-      )}
+        <div className={"flex"}>{leftColumn}</div>
+        <div
+          className="absolute top-1/2 right-0 transform -translate-y-1/2 flex items-center justify-center mt-12 h-10 bg-white rounded-full shadow cursor-pointer"
+          onClick={hideLeftColumn}
+        >
+          <ChevronLeftIcon className={"w-4 h-4 text-pink-500"} />
+        </div>
+      </div>
       <div className={isLeftColumnVisible ? "col-span-4" : "col-span-5"}>
         <div className={"w-full h-full flex-grow flex"}>
           {!isLeftColumnVisible && (
